8b69967f366fe1f2e214e861294e7a98f1326fc9
Chris Comeaux <chris.comeaux@gmail.com>
2014-02-28 20:05:55 -0600

    VersionPreference is not selectable

--- src/src/com/eaux/app/muzei/bgg/VersionPreference.java
+++ src/src/com/eaux/app/muzei/bgg/VersionPreference.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.preference.DialogPreference;
+import android.preference.Preference;
 import android.util.AttributeSet;
 
-public class VersionPreference extends DialogPreference {
+public class VersionPreference extends Preference {
+
+	public VersionPreference(Context context) {
+		super(context);
+	}
 
 	public VersionPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -15,6 +19,11 @@ public class VersionPreference extends DialogPreference {
 
 	public VersionPreference(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public boolean isSelectable() {
+		return false;
 	}
 
 	@Override
